0dd6fa369135c9a522cf84d2a1a946fa134f3a31
“Roshni7605” <roshnimali847@gmail.com>
2023-11-24 01:38:36 +0530

    Added: UC6-Playing the game with 2 players

--- src/snakeladdersimulator/SnakeAndLadder.java
+++ src/snakeladdersimulator/SnakeAndLadder.java
@@ -6,42 +6,56 @@ public class SnakeAndLadder {
 
         System.out.println("Welcome to Snake and Ladder Game!");
 
-        Player player = new Player();
+        Player player1 = new Player();
+        Player player2 = new Player();
         Dice dice = new Dice();
         GameBoard board = new GameBoard();
 
         int diceRollCount = 0;
+        int currentPlayer = 1;
 
-        while (player.getCurrentPosition() < 100){
+        while (player1.getCurrentPosition() < 100 && player2.getCurrentPosition() < 100){
+            Player currentPlayerObj = (currentPlayer == 1) ? player1 : player2;
+
             int diceValue = dice.rollDice();
             int option = dice.rollOption();
             diceRollCount++;
 
             switch (option){
                 case 1:
-                    System.out.println("No Play. Your current position is " + player.getCurrentPosition());
+                    System.out.println("No Play. Player " + currentPlayer + "'s current position is "
+                            + currentPlayerObj.getCurrentPosition());
                     break;
                 case 2:
-                    int newPosition = player.getCurrentPosition() + diceValue;
+                    int newPosition = currentPlayerObj.getCurrentPosition() + diceValue;
                     newPosition = board.movePlayerOnBoard(newPosition);
                     if (newPosition <= 100) {
-                        player.setCurrentPosition(newPosition);
+                        currentPlayerObj.setCurrentPosition(newPosition);
                     }
-                    System.out.println("You rolled a " + diceValue + ". Your current position is " + player.getCurrentPosition());
+                    System.out.println("Player " + currentPlayer + " rolled a " + diceValue + ". Current position is "
+                            + currentPlayerObj.getCurrentPosition());
                     break;
                 case 3:
-                    int snakePosition = player.getCurrentPosition() - diceValue;
+                    int snakePosition = currentPlayerObj.getCurrentPosition() - diceValue;
                     if (snakePosition < 0) {
                         snakePosition = 0;
                     }
-                    System.out.println("Oops! You rolled a snake. Going back to position " + snakePosition + ".");
-                    player.setCurrentPosition(snakePosition);
+                    System.out.println("Player " + currentPlayer + " rolled a snake. Going back to position "
+                            + snakePosition + ".");
+                    currentPlayerObj.setCurrentPosition(snakePosition);
                     break;
             }
-            
-            System.out.println("Congratulations! You reached the target position of 100");
-            System.out.println("Number of times the dice was played: " + diceRollCount);
 
+            // Switch to the next player only if the option is not a ladder
+            if (option != 2) {
+                currentPlayer = (currentPlayer == 1) ? 2 : 1;
+            }
+
+            if (player1.getCurrentPosition() == 100) {
+                System.out.println("Player 1 won the game!");
+            } else {
+                System.out.println("Player 2 won the game!");
+            }
         }
 
     }
